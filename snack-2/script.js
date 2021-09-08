4c14714f364e9a60d1f06b8f2cc5c3a2a116838f
alessandroNessi <alessandronessi@msn.com>
2021-09-08 11:57:57 +0200

    usato foreach

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,9 +2,6 @@ function popolaZucchine(){
     let zucchine = [];
     while(zucchine.length<10){
         var zucchina={ "tipo":"","peso":"","lunghezza":""};
-        // zucchina.tipo="";
-        // zucchina.peso="";
-        // zucchina.lunghezza="";
         console.log(zucchina);
         zucchina.tipo="zucchina"+Math.floor(Math.random()*100);
         zucchina.peso=Math.floor(Math.random()*200 +50)+"g";
@@ -15,15 +12,24 @@ function popolaZucchine(){
 }
 var zucchine= popolaZucchine();
 var zucchineCorte=[], zucchineLunghe=[], pesoCorte=0, pesoLunghe=0;
-for(i=0;i<zucchine.length;i++){
-    if(parseInt(zucchine[i].lunghezza.replace(/[a-z]/g,""))<15){
-        zucchineCorte.push(zucchine[i]);
-        pesoCorte+=parseInt(zucchine[i].peso.replace(/[a-z]/g,""));
+zucchine.forEach( element => {
+    if(parseInt(element.lunghezza.replace(/[a-z]/g,""))<15){
+        zucchineCorte.push(element);
+        pesoCorte+=parseInt(element.peso.replace(/[a-z]/g,""));
     }else{
-        zucchineLunghe.push(zucchine[i]);
-        pesoLunghe+=parseInt(zucchine[i].peso.replace(/[a-z]/g,""));
+        zucchineLunghe.push(element);
+        pesoLunghe+=parseInt(element.peso.replace(/[a-z]/g,""));
     }
-}
+});
+// for(i=0;i<zucchine.length;i++){
+//     if(parseInt(zucchine[i].lunghezza.replace(/[a-z]/g,""))<15){
+//         zucchineCorte.push(zucchine[i]);
+//         pesoCorte+=parseInt(zucchine[i].peso.replace(/[a-z]/g,""));
+//     }else{
+//         zucchineLunghe.push(zucchine[i]);
+//         pesoLunghe+=parseInt(zucchine[i].peso.replace(/[a-z]/g,""));
+//     }
+// }
 console.log(zucchineCorte);
 console.log(`peso zucchine corte:${pesoCorte}g`);
 console.log(zucchineLunghe);
